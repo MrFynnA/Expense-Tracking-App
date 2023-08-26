5eec3ce3d231d5e5aa7842ea1440fc858af824fe
Mr Fynn <104089609+MrFynnA@users.noreply.github.com>
2023-08-26 13:00:53 +0000

    edit section
    added edit section with tooltip

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,28 +3,45 @@ import './ExpenseItem.css';
 import './ExpenseDate.jsx';
 import Card from '../UI/Card.jsx';
 import ExpenseDate from './ExpenseDate.jsx';
+import {useDispatch,useSelector} from 'react-redux'
+import { uiAction } from '../expenseStore/expenseStore';
 
 
 
 
 const ExpenseItem=(props)=>{
+    const dispatch= useDispatch()
+    const displayState=useSelector(state=>state.uiState.uiChange)
+    const expenseID=useSelector(state=>state.uiState.expenseItemId)
     const title=props.expenseTitle
     const amount=props.expenseAmount
     const date=props.expenseDate
     const expenseId=props.expenseId
 
+    console.log(expenseID)
+
 const deleteHandle=()=>{
           props.expenseDel(expenseId)
 }
+const ondisplayEdit=()=>{
+    console.log(expenseId)
+    dispatch(uiAction.onexpenseEdit(expenseId))
+}
+
+let expenseDisplay=<span className='expenseTitle'>{title}</span>
+if(displayState && expenseID===expenseId){
+    expenseDisplay=<span className='expense_inputUP'><input type='text'></input></span>
+}
+
 
 
 return(
 <>
     <Card className='expense-item'>
-       
+    
        <ExpenseDate expenseDate={date}/>
         <div className='expense-item__description'>
-<h2>{title}</h2>
+<h2 onDoubleClick={ondisplayEdit}>{expenseDisplay}</h2>
 <div className='expense-item__price'>${amount}</div>
 <button  style={{backgroundColor:'#f56f3b'}} onClick={deleteHandle}><span style={{color:'white'}}>—</span></button>
         </div>
--- src/components/expenseStore/expenseStore.jsx
+++ src/components/expenseStore/expenseStore.jsx
@@ -5,20 +5,48 @@ import { createSlice, configureStore } from "@reduxjs/toolkit";
 const expenseSlice=createSlice({
     name:'expense',
     initialState:{
-        expenseTotal:0,
+        expenseItems:[],
     },
     reducers:{
-        onFilterdExpenseTotal(state,action){
-        state.expenseTotal=state.expenseTotal+action.payload.totalExpense
+        onReceivedItems(state,action){
+            console.log('dipatched')
+       const existingItem=state.expenseItems.find(items.id===action.payload.id)
+       if(existingItem){
+        existingItem.title=action.payload.title
+       }
         }
     }
 })
+const UISlice=createSlice({
+    name:'UI',
+    initialState:{
+        uiChange:false,
+        expenseItemId:''
+    },
+    reducers:{
+         onexpenseEdit(state,action){
+            console.log(action.payload)
+       state.uiChange=true
+       state.expenseItemId=action.payload
+         },
+         oneexpenseCloseEdit(state,action){
+            state.uiChange=false,
+            state.expenseId=state.expenseId=action.payload
+         }
+    }
+})
+console.log(UISlice.reducer.uiChange)
 
 
-export const expenseAction=expenseSlice.actions.onFilterdExpenseTotal
+export const expenseAction=expenseSlice.actions.onReceivedItems
+export const uiAction= UISlice.actions
 
 const store=configureStore({
-    reducer:expenseSlice.reducer
+    reducer:{
+        expenseState:expenseSlice.reducer,
+        uiState:UISlice.reducer
+    
+    }
 })
 
 export default store
